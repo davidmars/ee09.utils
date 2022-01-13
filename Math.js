e347fbf31a8044b3377ca228950dcefc232234a6
davidmars <d.marsalone@gmail.com>
2022-01-13 18:58:24 +0100

    ratio

--- Math.js
+++ Math.js
@@ -4,9 +4,22 @@
  * @param {Number} factor
  * @return {number}
  */
-export default function lerp(previous, target, factor){
+function lerp(previous, target, factor){
     return (1 - factor) * previous + factor * target;
 }
 
+/**
+ * Renvoie une valeur en fonction d'un autre espace de valeur
+ * @param {Number} input
+ * @param {Number} maxInput
+ * @param {Number} maxOutput
+ * @param {Number} minInput
+ * @param {Number} minOutput
+ * @return {Number}
+ */
+function ratio (input,minInput, maxInput,minOutput, maxOutput) {
+    let factor = (input - minInput) / (maxInput - minInput);
+    return ((maxOutput - minOutput) * factor) + minOutput;
+}
 
-console.log("EE09Math imported");
+export {lerp,ratio};
