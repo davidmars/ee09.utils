9a91c464c3aed7813b5281b71de33c92d2c43b5a
davidmars <d.marsalone@gmail.com>
2022-01-13 10:15:48 +0100

    refactor Math.lerp

---
+++ Math.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Number} previous
+ * @param {Number} target
+ * @param {Number} factor
+ * @return {number}
+ */
+export default function lerp(previous, target, factor){
+    return (1 - factor) * previous + factor * target;
+}
+
+
+console.log("EE09Math imported");
