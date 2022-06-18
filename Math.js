a9be098eb3946c3968b2c466e1d0f310b7e10fef
davidmars <d.marsalone@gmail.com>
2022-06-18 07:56:48 +0200

    fix export

--- Math.js
+++ Math.js
@@ -2,7 +2,7 @@ import lerp from "./Math/lerp.js";
 import ratio from "./Math/ratio.js";
 import rand from "./Math/rand.js";
 import deg2rad from "./Math/deg2rad.js";
-export default {
+export {
     lerp,
     ratio,
     rand,
